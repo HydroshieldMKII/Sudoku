d917facfe5e20cefea5bc2aeac8e1ebea731a8ad
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-11-08 11:04:46 -0500

    IsSafe_Step2

--- Sudoku/Sudoku/Validations.cs
+++ Sudoku/Sudoku/Validations.cs
@@ -32,6 +32,42 @@ namespace Sudoku
             return true;
         }
 
+        private bool IsSafe(int[,] grid, int row, int col, int number)
+        {
+            //Check if the number is already in the row
+            for (int i = 0; i < 9; i++)
+            {
+                if (grid[row, i] == number)
+                {
+                    return false;
+                }
+            }
+
+            //Check if the number is already in the column
+            for (int i = 0; i < 9; i++)
+            {
+                if (grid[i, col] == number)
+                {
+                    return false;
+                }
+            }
+
+            //Check if the number is already in the square
+            int startRow = row - row % 3;
+            int startCol = col - col % 3;
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    if (grid[i, j] == number)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
         private bool IsNumberInRow(int[,] grid, int row, int col)
         {
             int number = grid[row, col];
