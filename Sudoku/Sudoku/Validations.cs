bd73071d1efab63fc3964b9b896dccd3cfd547cd
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-11-08 10:53:31 -0500

    IsValid_Steps2

--- Sudoku/Sudoku/Validations.cs
+++ Sudoku/Sudoku/Validations.cs
@@ -9,45 +9,45 @@ namespace Sudoku
 {
     public class Validations
     {
-        //public bool IsValid(int[,] grid, int row, int col)
-        //{
-        //    // Vérifie si le nombre est déjà dans la ligne
-        //    for (int i = 0; i < 9; i++)
-        //    {
-        //        if (i != col && grid[row, i] == grid[row, col])
-        //        {
-        //            Console.WriteLine("Le nombre est déjà dans la ligne");
-        //            return false;
-        //        }
-        //    }
+        public bool IsValid(int[,] grid, int row, int col)
+        {
+            // Vérifie si le nombre est déjà dans la ligne
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != col && grid[row, i] == grid[row, col])
+                {
+                    Console.WriteLine("Le nombre est déjà dans la ligne");
+                    return false;
+                }
+            }
 
-        //    // Vérifie si le nombre est déjà dans la colonne
-        //    for (int i = 0; i < 9; i++)
-        //    {
-        //        if (i != row && grid[i, col] == grid[row, col])
-        //        {
-        //            Console.WriteLine("Le nombre est déjà dans la colonne");
-        //            return false;
-        //        }
-        //    }
+            // Vérifie si le nombre est déjà dans la colonne
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != row && grid[i, col] == grid[row, col])
+                {
+                    Console.WriteLine("Le nombre est déjà dans la colonne");
+                    return false;
+                }
+            }
 
-        //    // Vérifie si le nombre est déjà dans le carré
-        //    int startRow = row - row % 3;
-        //    int startCol = col - col % 3;
-        //    for (int i = startRow; i < startRow + 3; i++)
-        //    {
-        //        for (int j = startCol; j < startCol + 3; j++)
-        //        {
-        //            if (i != row && j != col && grid[i, j] == grid[row, col])
-        //            {
-        //                Console.WriteLine("Le nombre est déjà dans le carré");
-        //                return false;
-        //            }
-        //        }
-        //    }
+            // Vérifie si le nombre est déjà dans le carré
+            int startRow = row - row % 3;
+            int startCol = col - col % 3;
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    if (i != row && j != col && grid[i, j] == grid[row, col])
+                    {
+                        Console.WriteLine("Le nombre est déjà dans le carré");
+                        return false;
+                    }
+                }
+            }
 
-        //    return true;
-        //}
+            return true;
+        }
 
         //public bool IsSafe(int[,] grid, int row, int col, int num)
         //{
