ba364981a8d4bd96dbfd99158b6bd5201ed3f655
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-11-16 20:16:54 -0500

    IsGameWon_Step1

--- Sudoku/Sudoku/Game.cs
+++ Sudoku/Sudoku/Game.cs
@@ -15,6 +15,53 @@ namespace Sudoku
         {
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
-        }
+
+            printServices.PrintMainMenu();
+
+            while (true)
+            {
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        sudoku.GenerateGrid();
+                        sudoku.InitialGrid = sudoku.Grid.Clone() as int[,];
+                        printServices.PrintGrid(sudoku.Grid, sudoku.InitialGrid);
+                        break;
+                    case "2":
+                        sudoku.Grid = sFile.LoadGrid();
+                        sudoku.InitialGrid = sudoku.Grid.Clone() as int[,];
+                        printServices.PrintGrid(sudoku.Grid, sudoku.InitialGrid);
+                        break;
+                    case "3":
+                        printServices.PrintDifficultySelect(sudoku.Difficulty);
+                        string diffChoice = Console.ReadLine();
+                        switch (diffChoice)
+                        {
+                            case "1":
+                                sudoku.Difficulty = 30;
+                                break;
+                            case "2":
+                                sudoku.Difficulty = 40;
+                                break;
+                            case "3":
+                                sudoku.Difficulty = 50;
+                                break;
+                            case "4":
+                                sudoku.Difficulty = 60;
+                                break;
+                            default:
+                                Console.WriteLine("Choix invalide");
+                                break;
+                        }
+                        break;
+                    case "4":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Choix invalide");
+                        break;
+                }
+            }
     }
 }
