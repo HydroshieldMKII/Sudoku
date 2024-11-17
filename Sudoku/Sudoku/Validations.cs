7289bdaa7e0d8e3cb785ee12afe5c5ab1f77ffae
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-11-16 20:22:32 -0500

    Revert "IsGameWon_Step3"
    This reverts commit 96f99c08ec1200622006f42ebb92ddb7de8fec84.

--- Sudoku/Sudoku/Validations.cs
+++ Sudoku/Sudoku/Validations.cs
@@ -112,20 +112,5 @@ namespace Sudoku
 
             return true;
         }
-    
-        public bool IsGameWon(int[,] grid)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (grid[i, j] == 0)
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
     }
 }
